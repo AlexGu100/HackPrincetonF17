eb0e613f7ed59bbc5fa5b19cf8905981a2fd59b4
AlexGu100 <alexgu100@gmail.com>
2017-11-11 16:52:55 -0500

    Commit 1

--- app/src/main/java/com/scarletsteel/hackprincetonf17/CameraActivity.java
+++ app/src/main/java/com/scarletsteel/hackprincetonf17/CameraActivity.java
@@ -8,35 +8,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity {
 
-    private Camera mCamera = null;
-    private CameraView mCameraView = null;
+    private Camera mCamera;
+    private CameraView mCameraView;
+    private TextureView previewTextureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1337 );
 
-        try{
-            mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e){
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-        }
-
-        if(mCamera != null) {
-            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
-        }
+        previewTextureView = ((TextureView)findViewById(R.id.camera_textureview_preview));
 
         /*//btn to close the application
         ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
@@ -46,5 +39,27 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });*/
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to open camera: " + e.getMessage());
+        }
+
+        Camera.PreviewCallback processFrame = new Camera.PreviewCallback()
+        {
+            public void onPreviewFrame(byte[] previewData, Camera previewCamera)
+            {
+                mCameraView.previewCallbackCompleted();
+            }
+        };
+
+        mCameraView = new CameraView(this, mCamera, previewTextureView, this, processFrame);
     }
 }
--- app/src/main/java/com/scarletsteel/hackprincetonf17/CameraView.java
+++ app/src/main/java/com/scarletsteel/hackprincetonf17/CameraView.java
@@ -2,6 +2,8 @@ package com.scarletsteel.hackprincetonf17;
 
 import java.io.IOException;
 import android.content.Context;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.util.Log;
 import android.graphics.Canvas;
@@ -9,64 +11,84 @@ import android.graphics.Rect;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.TextureView;
 
-public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
-
-    private SurfaceHolder mHolder;
+public class CameraView extends TextureView implements TextureView.SurfaceTextureListener
+{
+    private CameraActivity mCameraActivity;
     private Camera mCamera;
+    private Camera.PreviewCallback mPreviewCallback;
+    private TextureView mTextureView;
+    private byte[] cameraData;
 
-    public CameraView(Context context, Camera camera){
+    public CameraView(Context context, Camera camera, TextureView textureView, final CameraActivity cameraActivity, Camera.PreviewCallback previewCallback) {
         super(context);
-
         mCamera = camera;
-        mCamera.setDisplayOrientation(90);
-        //get the holder and set this class as the callback, so we can get camera data here
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+        mCameraActivity = cameraActivity;
+        mPreviewCallback = previewCallback;
+        mTextureView = textureView;
+        mTextureView.setSurfaceTextureListener(this);
+
+        if (this.mTextureView.isAvailable()) {
+            onSurfaceTextureAvailable(mTextureView.getSurfaceTexture(), mTextureView.getWidth(), mTextureView.getHeight());
+        }
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        try{
-            Camera.Parameters params = mCamera.getParameters();
-            params.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);
-            mCamera.setParameters(params);
+    private byte[] getPreviewBuffer() {
+        int bufferSize;
+        Camera.Parameters parameters = this.mCamera.getParameters();
+        int previewFormat = parameters.getPreviewFormat();
+        Camera.Size previewSize = parameters.getPreviewSize();
+        if (previewFormat != 842094169) {
+            bufferSize = (int) (((float) (previewSize.width * previewSize.height)) * (((float) ImageFormat.getBitsPerPixel(previewFormat)) / 8.0f));
+        } else {
+            int yStride = ((int) Math.ceil(((double) previewSize.width) / 16.0d)) * 16;
+            bufferSize = (yStride * previewSize.height) + (((previewSize.height * (((int) Math.ceil(((double) (yStride / 2)) / 16.0d)) * 16)) / 2) * 2);
+        }
+        return new byte[bufferSize];
+    }
 
-            //when the surface is created, we can set the camera to draw images in this surfaceholder
-            mCamera.setPreviewDisplay(surfaceHolder);
+    public void previewCallbackCompleted()
+    {
+        mCamera.addCallbackBuffer(cameraData);
+    }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture paramSurfaceTexture, int width, int height)
+    {
+        try
+        {
+            // Temp orientation hack
+            mCamera.setDisplayOrientation(90);
+
+            mCamera.setPreviewTexture(paramSurfaceTexture);
+            cameraData = getPreviewBuffer();
+            mCamera.addCallbackBuffer(cameraData);
+            mCamera.setPreviewCallbackWithBuffer(mPreviewCallback);
             mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d("ERROR", "Camera error on surfaceCreated " + e.getMessage());
+        }
+        catch (IOException e)
+        {
+            // Exception
         }
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-        //before changing the application orientation, you need to stop the preview, rotate and then start it again
-        if(mHolder.getSurface() == null)//check if the surface is ready to receive camera data
-            return;
-
-        try{
-            mCamera.stopPreview();
-        } catch (Exception e){
-            //this will happen when you are trying the camera if it's not running
-        }
-
-        //now, recreate the camera preview
-        try{
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d("ERROR", "Camera error on surfaceChanged " + e.getMessage());
-        }
+    public void onSurfaceTextureSizeChanged(SurfaceTexture paramSurfaceTexture, int paramInt1, int paramInt2)
+    {
+        //
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        //our app has only one screen, so we'll destroy the camera in the surface
-        //if you are unsing with more screens, please move this code your activity
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture)
+    {
         mCamera.stopPreview();
         mCamera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface)
+    {
+        //
     }
 }
